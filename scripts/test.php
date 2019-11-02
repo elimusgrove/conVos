2c0ddac944c6cf095689ef8776a509f16b467abf
Eli Musgrove <eli.musgrove9@gmail.com>
2019-11-02 01:15:39 -0500

    Testing.

--- scripts/test.php
+++ scripts/test.php
@@ -28,28 +28,29 @@ use Google\Cloud\Language\V1\Entity\Type as EntityType;
 
 $languageServiceClient = new LanguageServiceClient(['keyFile' => json_decode(file_get_contents("/home3/hsnkwamy/public_html/vendor/SiriButWorse-0e5d6a5f7218.json"), true)]);
 
-try {
-    // Create a new Document, add text as content and set type to PLAIN_TEXT
-    $document = (new Document())
-        ->setContent($_GET['string'])
-        ->setType(Type::PLAIN_TEXT);
-
-    // Call the analyzeEntities function
-    $response = $languageServiceClient->analyzeEntities($document, []);
-    $entities = $response->getEntities();
-    // Print out information about each entity
-    foreach ($entities as $entity) {
-        printf('Name: %s' . PHP_EOL, $entity->getName());
-        printf('Type: %s' . PHP_EOL, EntityType::name($entity->getType()));
-        printf('Salience: %s' . PHP_EOL, $entity->getSalience());
-        if ($entity->getMetadata()->offsetExists('wikipedia_url')) {
-            printf('Wikipedia URL: %s' . PHP_EOL, $entity->getMetadata()->offsetGet('wikipedia_url'));
-        }
-        if ($entity->getMetadata()->offsetExists('mid')) {
-            printf('Knowledge Graph MID: %s' . PHP_EOL, $entity->getMetadata()->offsetGet('mid'));
-        }
-        printf(PHP_EOL);
-    }
-} finally {
-    $languageServiceClient->close();
-}
+//$text = $_GET['string'];
+//try {
+//    // Create a new Document, add text as content and set type to PLAIN_TEXT
+//    $document = (new Document())
+//        ->setContent($text)
+//        ->setType(Type::PLAIN_TEXT);
+//
+//    // Call the analyzeEntities function
+//    $response = $languageServiceClient->analyzeEntities($document, []);
+//    $entities = $response->getEntities();
+//    // Print out information about each entity
+//    foreach ($entities as $entity) {
+//        printf('Name: %s' . PHP_EOL, $entity->getName());
+//        printf('Type: %s' . PHP_EOL, EntityType::name($entity->getType()));
+//        printf('Salience: %s' . PHP_EOL, $entity->getSalience());
+//        if ($entity->getMetadata()->offsetExists('wikipedia_url')) {
+//            printf('Wikipedia URL: %s' . PHP_EOL, $entity->getMetadata()->offsetGet('wikipedia_url'));
+//        }
+//        if ($entity->getMetadata()->offsetExists('mid')) {
+//            printf('Knowledge Graph MID: %s' . PHP_EOL, $entity->getMetadata()->offsetGet('mid'));
+//        }
+//        printf(PHP_EOL);
+//    }
+//} finally {
+//    $languageServiceClient->close();
+//}
