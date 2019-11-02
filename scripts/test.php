b63d2095436ac95f502fc965badeec211e68739c
Eli Musgrove <eli.musgrove9@gmail.com>
2019-11-01 22:51:17 -0500

    Print the get or post request in upper case.

--- scripts/test.php
+++ scripts/test.php
@@ -1,11 +1,11 @@
 <?php
 
 if (isset($_GET)) {
-    var_dump(array(strtoupper($_GET['string'])));
+    echo json_encode(array(strtoupper($_GET['string'])));
 }
 
 if (isset($_POST)) {
-    var_dump(array(strtoupper($_POST['string'])));
+    json_encode(array(strtoupper($_POST['string'])));
 }
 
 ?>
