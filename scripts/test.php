e2599d181b2b03d6ce81d050b33efe20c2ec77bc
Eli Musgrove <eli.musgrove9@gmail.com>
2019-11-02 01:01:38 -0500

    Testing.

--- scripts/test.php
+++ scripts/test.php
@@ -4,18 +4,18 @@
 //    echo json_encode(array('value'=>array(strtoupper($_GET['string']), 'value 2')));
 //}
 
-# Includes the autoloader for libraries installed with composer
+// Includes the autoloader for libraries installed with composer
 require '/home3/hsnkwamy/public_html/vendor/autoload.php';
 
-# Imports the Google Cloud client library
+// Imports the Google Cloud client library
 use Google\Cloud\Language\LanguageClient;
 $language = new LanguageClient(['keyFile' => json_decode(file_get_contents("/home3/hsnkwamy/public_html/vendor/SiriButWorse-0e5d6a5f7218.json"), true)]);
 
-
-# The text to analyze
+var_dump($language) . PHP_EOL;
+// The text to analyze
 $text = $_GET['string'];
 
-# Detects the sentiment of the text
+// Detects the sentiment of the text
 $annotation = $language->analyzeSentiment($text);
 $sentiment = $annotation->sentiment();
 
