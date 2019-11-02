f6b543e0aceb37a9037b942fcc9e6bebdf5f0c90
Eli Musgrove <eli.musgrove9@gmail.com>
2019-11-02 15:31:17 -0500

    Update.

--- scripts/request.php
+++ scripts/request.php
@@ -107,16 +107,16 @@ if (isset($_GET['keyword'])) {
         $result = curl_exec($curl);
         curl_close($curl);
 
-        echo $result;
+//        echo $result;
 
         // Filter html
         $dom_results = new simple_html_dom();
         $dom_results->load($result);
 
-        // Get paragraph elements
-//        foreach($dom_results->find('p') as $par) {
-//            echo $par->plaintext . '<br/>';
-//        }
+//         Get paragraph elements
+        foreach($dom_results->find('.BNeawe\s3v9rd\AP7Wnd') as $par) {
+            echo $par->plaintext . '<br/>';
+        }
 
     }
 }
