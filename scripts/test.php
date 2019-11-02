a7184680b749fad8735e8429d3221ac6331cc782
Eli Musgrove <eli.musgrove9@gmail.com>
2019-11-02 00:35:12 -0500

    Add API key.

--- scripts/test.php
+++ scripts/test.php
@@ -9,9 +9,25 @@ require '/home3/hsnkwamy/public_html/vendor/autoload.php';
 
 # Imports the Google Cloud client library
 use Google\Cloud\Language\LanguageClient;
+use Google\Cloud\Storage\StorageClient;
+
+// Authenticating with keyfile data.
+$storage = new StorageClient([
+    'keyFile' => json_decode(file_get_contents('/home3/hsnkwamy/public_html/vendor/SiriButWorse-0e5d6a5f7218.json'), true)
+]);
+
+// Authenticating with a keyfile path.
+$storage = new StorageClient([
+    'keyFilePath' => '/home3/hsnkwamy/public_html/vendor/SiriButWorse-0e5d6a5f7218.json'
+]);
+
+// Providing the Google Cloud project ID.
+$storage = new StorageClient([
+    'projectId' => 'SiriButWorse'
+]);
 
 # Your Google Cloud Platform project ID
-$projectId = 'YOUR_PROJECT_ID';
+$projectId = 'siributworse-1572669643321';
 
 # Instantiates a client
 $language = new LanguageClient([
