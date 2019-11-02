5526b0d188c2445cfa19cdcb93501181178c22ab
Eli Musgrove <eli.musgrove9@gmail.com>
2019-11-02 02:09:27 -0500

    Testing.

--- scripts/test.php
+++ scripts/test.php
@@ -9,18 +9,51 @@ putenv("GOOGLE_APPLICATION_CREDENTIALS=/home3/hsnkwamy/public_html/vendor/SiriBu
 // Includes the autoloader for libraries installed with composer
 require '/home3/hsnkwamy/public_html/vendor/autoload.php';
 
-// Imports the Google Cloud client library
-use Google\Cloud\Language\LanguageClient;
-$language = new LanguageClient();
+use Google\Cloud\Language\V1\Document;
+use Google\Cloud\Language\V1\Document\Type;
+use Google\Cloud\Language\V1\LanguageServiceClient;
+use Google\Cloud\Language\V1\Entity\Type as EntityType;
 
-// The text to analyze
 $text = $_GET['string'];
 
-// Detects the sentiment of the text
-$annotation = $language->analyzeSentiment($text);
-$sentiment = $annotation->sentiment();
+// Create the Natural Language client
+$languageServiceClient = new LanguageServiceClient();
+try {
+    // Create a new Document, add text as content and set type to PLAIN_TEXT
+    $document = (new Document())
+        ->setContent($text)
+        ->setType(Type::PLAIN_TEXT);
 
-echo 'Text: ' . $text . '
-Sentiment: ' . $sentiment['score'] . ', ' . $sentiment['magnitude'];
+    // Call the analyzeEntities function
+    $response = $languageServiceClient->analyzeEntities($document, []);
+    $entities = $response->getEntities();
+    // Print out information about each entity
+    foreach ($entities as $entity) {
+        printf('Name: %s' . PHP_EOL, $entity->getName());
+        printf('Type: %s' . PHP_EOL, EntityType::name($entity->getType()));
+        printf('Salience: %s' . PHP_EOL, $entity->getSalience());
+        if ($entity->getMetadata()->offsetExists('wikipedia_url')) {
+            printf('Wikipedia URL: %s' . PHP_EOL, $entity->getMetadata()->offsetGet('wikipedia_url'));
+        }
+        if ($entity->getMetadata()->offsetExists('mid')) {
+            printf('Knowledge Graph MID: %s' . PHP_EOL, $entity->getMetadata()->offsetGet('mid'));
+        }
+        printf(PHP_EOL);
+    }
+} finally {
+    $languageServiceClient->close();
+}
 
-// ['keyFile' => json_decode(file_get_contents("/home3/hsnkwamy/public_html/vendor/SiriButWorse-0e5d6a5f7218.json"), true)]
+//// Imports the Google Cloud client library
+//use Google\Cloud\Language\LanguageClient;
+//$language = new LanguageClient();
+//
+//// The text to analyze
+//$text = $_GET['string'];
+//
+//// Detects the sentiment of the text
+//$annotation = $language->analyzeSentiment($text);
+//$sentiment = $annotation->sentiment();
+//
+//echo 'Text: ' . $text . '
+//Sentiment: ' . $sentiment['score'] . ', ' . $sentiment['magnitude'];
