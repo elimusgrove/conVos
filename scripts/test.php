102445a2e860420e6977874f77087c07d15deac3
Eli Musgrove <eli.musgrove9@gmail.com>
2019-11-02 01:32:28 -0500

    Testing.

--- scripts/test.php
+++ scripts/test.php
@@ -5,7 +5,7 @@
 //}
 
 //// Includes the autoloader for libraries installed with composer
-require '/home3/hsnkwamy/public_html/vendor/autoload.php';
+//require '/home3/hsnkwamy/public_html/vendor/autoload.php';
 //
 //// Imports the Google Cloud client library
 //use Google\Cloud\Language\LanguageClient;
@@ -21,36 +21,20 @@ require '/home3/hsnkwamy/public_html/vendor/autoload.php';
 //echo 'Text: ' . $text . '
 //Sentiment: ' . $sentiment['score'] . ', ' . $sentiment['magnitude'];
 
-use Google\Cloud\Language\V1\Document;
-use Google\Cloud\Language\V1\Document\Type;
-use Google\Cloud\Language\V1\LanguageServiceClient;
-use Google\Cloud\Language\V1\Entity\Type as EntityType;
+namespace Google\Cloud\Samples\Auth;
 
-$languageServiceClient = new LanguageServiceClient(['keyFile' => json_decode(file_get_contents("/home3/hsnkwamy/public_html/vendor/SiriButWorse-0e5d6a5f7218.json"), true)]);
+// Imports the Cloud Storage client library.
+use Google\Cloud\Storage\StorageClient;
 
-//$text = $_GET['string'];
-//try {
-//    // Create a new Document, add text as content and set type to PLAIN_TEXT
-//    $document = (new Document())
-//        ->setContent($text)
-//        ->setType(Type::PLAIN_TEXT);
-//
-//    // Call the analyzeEntities function
-//    $response = $languageServiceClient->analyzeEntities($document, []);
-//    $entities = $response->getEntities();
-//    // Print out information about each entity
-//    foreach ($entities as $entity) {
-//        printf('Name: %s' . PHP_EOL, $entity->getName());
-//        printf('Type: %s' . PHP_EOL, EntityType::name($entity->getType()));
-//        printf('Salience: %s' . PHP_EOL, $entity->getSalience());
-//        if ($entity->getMetadata()->offsetExists('wikipedia_url')) {
-//            printf('Wikipedia URL: %s' . PHP_EOL, $entity->getMetadata()->offsetGet('wikipedia_url'));
-//        }
-//        if ($entity->getMetadata()->offsetExists('mid')) {
-//            printf('Knowledge Graph MID: %s' . PHP_EOL, $entity->getMetadata()->offsetGet('mid'));
-//        }
-//        printf(PHP_EOL);
-//    }
-//} finally {
-//    $languageServiceClient->close();
-//}
+# Explicitly use service account credentials by specifying the private key
+# file.
+$config = [
+    'keyFilePath' => "/home3/hsnkwamy/public_html/vendor/SiriButWorse-0e5d6a5f7218.json",
+    'projectId' => "siributworse-1572669643321",
+];
+$storage = new StorageClient($config);
+
+# Make an authenticated API request (listing storage buckets)
+foreach ($storage->buckets() as $bucket) {
+    printf('Bucket: %s' . PHP_EOL, $bucket->name());
+}
