b4a556ff3b0dbf0331ef1b7fd5c92bafb6466d51
Eli Musgrove <eli.musgrove9@gmail.com>
2019-11-02 00:19:56 -0500

    Print the get or post request in upper case.

--- scripts/test.php
+++ scripts/test.php
@@ -1,7 +1,31 @@
 <?php
 
 if (isset($_GET)) {
-    echo json_encode(array('value'=>array(strtoupper($_GET['string']), 'Conley has no balls')));
+    echo json_encode(array('value'=>array(strtoupper($_GET['string']), 'value 2')));
 }
+
+# Includes the autoloader for libraries installed with composer
+require __DIR__ . '/vendor/autoload.php';
+
+# Imports the Google Cloud client library
+use Google\Cloud\Language\LanguageClient;
+
+# Your Google Cloud Platform project ID
+$projectId = 'YOUR_PROJECT_ID';
+
+# Instantiates a client
+$language = new LanguageClient([
+    'projectId' => $projectId
+]);
+
+# The text to analyze
+$text = 'Hello, world!';
+
+# Detects the sentiment of the text
+$annotation = $language->analyzeSentiment($text);
+$sentiment = $annotation->sentiment();
+
+echo 'Text: ' . $text . '
+Sentiment: ' . $sentiment['score'] . ', ' . $sentiment['magnitude'];
 
 ?>
